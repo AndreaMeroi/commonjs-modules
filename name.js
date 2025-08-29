08f0495d4fd346eb72dd12dcc56fc57074fab16f
AndreaMeroi <andreameroidublin@gmail.com>
2025-08-29 10:52:09 +0200

    add obj name

---
+++ name.js
@@ -0,0 +1,13 @@
+function fullName(firstName, lastName) {
+
+    const fullName = {
+        firstName: '',
+        lastName: ''
+    }
+
+    return fullName
+}
+
+const NameComplete = fullName('', '')
+
+console.log(NameComplete)
