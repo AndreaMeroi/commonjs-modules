513f74427f2e865bc5cfcd96b250516a2b1d5031
AndreaMeroi <andreameroidublin@gmail.com>
2025-08-29 10:56:39 +0200

    add array hobbies

---
+++ hobbies.js
@@ -0,0 +1,10 @@
+function hobbieslist(hobbyOne, hobbyTwo, hobbyThree) {
+
+    const hobbiesList = ['bricolage', 'trekking', 'painting']
+
+    return hobbiesList
+}
+
+const fullHobbiesList = hobbieslist('')
+
+console.log(fullHobbiesList);
